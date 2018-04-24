fde336ce025366889fc46a14a8d59547b932a496
thynguyen <thynguyen222@gmail.com>
2018-04-25 01:31:07 +0700

    update

--- resources/views/front/partial/download_button.blade.php
+++ resources/views/front/partial/download_button.blade.php
@@ -3,5 +3,5 @@
         <a href="javascript:void(0);" onclick="fncDownload(this);" data-id="{{ $file->id }}" class="btn btn_03">Cloud</a>
     @endif
 @else
-<a href="javascript:void(0);" onclick="fncDownload(this);" data-id="{{ $file->id }}" class="btn btn_03">download</a>
+<a href="javascript:void(0);" data-toggle="modal" data-target="#myModal" class="btn btn_03">download</a>
 @endif
